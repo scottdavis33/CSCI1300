8ec3511ad9a0e2ca8b319bab0defbde4be3f1850
jpreyer7 <117961188+jpreyer7@users.noreply.github.com>
2022-11-14 22:03:40 -0700

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,7 +1,7 @@
 // CSCI 1300 Fall 2022
-// Author: Scott Davis
-// Recitation: 308 – Baljot Kaur
-// Project 3 - Class 4
+// Author: Jared Preyer && Scott Davis 
+// Recitation: 105 - Reagan Rychecky && 308 - Baljot Kaur
+// Project 3 - Class Player
 #include <iostream>
 #ifndef Player_H
 #define Player_H
