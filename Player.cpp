b4e5ccb6407e0414daf7011e7ceeb133691f48e3
scottdavis33 <117961419+scottdavis33@users.noreply.github.com>
2022-11-25 12:52:56 -0700

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,7 +1,7 @@
 // CSCI 1300 Fall 2022
-// Author: Jared Preyer && Scott Davis 
-// Recitation: 105 - Reagan Rychecky && 308 - Baljot Kaur
-// Project 3 - Class Player
+// Author: Scott Davis
+// Recitation: 308 – Baljot Kaur
+// Project 3 - Class 4
 #include <iostream>
 #include "Player.h"
 #include <cstdlib>
@@ -25,14 +25,14 @@ Psedocode for parameterized constructor
 */
 Player::Player(int fullness, string name, bool life)
 {
-   fullness_ = fullness;
-   name_ = name;
-   life_ = life;
+    fullness_ = fullness;
+    name_ = name;
+    life_ = life;
 }
-// /*
-// Pseudocode for the getters()
-// 1. return the private variables
-// */
+/*
+Pseudocode for the getters()
+1. return the private variables
+*/
 int Player::getFullness()
 {
     return fullness_;
@@ -45,14 +45,10 @@ bool Player::getLife()
 {
     return life_;
 }
-// /*
+
 // Pseudocode for the setters (fullness, Name, life)
 // 1. set private variables to parameters
-// */
-void Player::setName(string name)
-{
-    name_ = name;
-}
+
 void Player::setLife(bool life)
 {
     life_ = life;
@@ -61,15 +57,29 @@ void Player::setFullness(int fullness)
 {
     fullness_ = fullness;
 }
-// /*
-// Psuedocode movement
-// 1. Using a random number to get the probability of each player move
-// 2. If it == 4 then we need to take 1 from their fullness
-// */
+void Player::setName(string name)
+{
+    name_ = name;
+}
+/*
+Psuedocode movement
+1. Using a random number to get the probability of each player move
+2. If it == 4 then we need to take 1 from their fullness
+*/
 void Player::movement()
 {
-    if(rand() % 5 == 4)
+    if(rand()%5 == 2)
     {
         fullness_ = fullness_ - 1;
     }
+}
+
+void Player::setWeapon(int weapon)
+{
+    weapon_ = weapon;
+}
+
+int Player::getWeapon()
+{
+    return weapon_;
 }
