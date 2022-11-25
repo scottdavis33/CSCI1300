69b9c804677597ce6cdeef290f14a385b0e4a952
scottdavis33 <117961419+scottdavis33@users.noreply.github.com>
2022-11-25 12:53:08 -0700

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -3,83 +3,45 @@
 // Recitation: 308 – Baljot Kaur
 // Project 3 - Class 4
 #include <iostream>
-#include "Player.h"
-#include <cstdlib>
-#include <string>
+#ifndef Player_H
+#define Player_H
 using namespace std;
-/*
-Psedocode for Player default constructor
-1. set string to be empty
-2. set fullness to 50 (each player)
-3. set bool to true
-*/
-Player::Player()
-{
-    fullness_ = 50; // respected fullness for each player
-    name_ = ""; // will be prompted for all 5 players
-    life_ = true; // initail will be set to true unless they die
-}
-/*
-Psedocode for parameterized constructor
-1. Set private variables to parameters
-*/
-Player::Player(int fullness, string name, bool life)
-{
-    fullness_ = fullness;
-    name_ = name;
-    life_ = life;
-}
-/*
-Pseudocode for the getters()
-1. return the private variables
-*/
-int Player::getFullness()
-{
-    return fullness_;
-}
-string Player::getName()
-{
-    return name_;
-}
-bool Player::getLife()
-{
-    return life_;
-}
 
-// Pseudocode for the setters (fullness, Name, life)
-// 1. set private variables to parameters
-
-void Player::setLife(bool life)
-{
-    life_ = life;
-}
-void Player::setFullness(int fullness)
-{
-    fullness_ = fullness;
-}
-void Player::setName(string name)
-{
-    name_ = name;
-}
 /*
-Psuedocode movement
-1. Using a random number to get the probability of each player move
-2. If it == 4 then we need to take 1 from their fullness
+Pseudocode Player Class
+1. Make your private variables that you will need
+2. Get your public functions
+3. Up and running
 */
-void Player::movement()
-{
-    if(rand()%5 == 2)
-    {
-        fullness_ = fullness_ - 1;
-    }
-}
 
-void Player::setWeapon(int weapon)
+class Player
 {
-    weapon_ = weapon;
-}
+// Player Objects
+private:
 
-int Player::getWeapon()
-{
-    return weapon_;
-}
+    int fullness_;
+    string name_;
+    bool life_;
+    int weapon_ = 0;
+
+public:
+    // default and parameterized constructor
+    Player();
+    Player(int fullness_, string name_, bool life_);
+    
+    // Getters
+    int getFullness();
+    string getName();
+    bool getLife();
+    
+    // Setters
+    void setName(string name);
+    void setLife(bool life);
+    void setFullness(int fullness);
+    int getWeapon();
+    void setWeapon(int);
+    // for moving each individual player
+    void movement();
+
+};
+#endif
