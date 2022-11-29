22f0d463dc59d43d841481b390cf804f5aa22273
jpreyer7 <117961188+jpreyer7@users.noreply.github.com>
2022-11-28 22:24:49 -0700

    Update Dungeon.cpp

--- Dungeon.cpp
+++ Dungeon.cpp
@@ -24,14 +24,8 @@ int main()
     
     do
     {
-        cout << "Choose one of the following:" << endl
-        << "1. Cookware: You will need something to cook those ingredients." << endl
-        << "2. Ingredients: To make food, you have to cook raw ingredients." << endl
-        << "3. Weapons: It's dangerous to go alone, take this!" << endl
-        << "4. Armor: If you want to survive monster attacks, you will need some armor." << endl
-        << "5. Sell treasures: If you find anything shiny, I would be happy to take it off your hands." << endl
-        << "6. Leave: Make sure you get everything you need. I'm leaving after this sale!" << endl;
-        cin >>options;
+        M.InventoryMenu();
+        cin >> options;
         Eat.merchant_.displayMenu();
         
         /* 
@@ -44,29 +38,36 @@ int main()
             case 1:
             {
                 Eat.merchant_.setCookware();
+                Eat.merchant_.displayMenu();
                 break;
             }
             case 2:
             {
                 Eat.merchant_.setIngredients();
+                Eat.merchant_.displayMenu();
                 break;
             }
             case 3:
             {
                 Eat.merchant_.setWeapons(G);
+                Eat.merchant_.displayMenu();
                 break;
             }
             case 4:
             {
                 Eat.merchant_.setArmor(G);
+                Eat.merchant_.displayMenu();
                 break;
             }
             case 5:
             {
                 Eat.merchant_.setsTreasures();
+                Eat.merchant_.displayMenu();
+                break;
             }
             default:
             cout << "Enjoy your journey team!" << endl;
+            Eat.merchant_.displayMenu();
             break;
         }
 
@@ -87,6 +88,11 @@ int main()
                 cout << "What direction would you like to move in? (wasd)" << endl;
                 cin >> choice1;
                 Eat.move(choice1, map);
+                if(map.isNPCLocation(map.getPlayerRow(), map.getPlayerCol()) == true)
+                {
+                    Eat.NPCEncounter(G, map);
+                }
+
                 break;
             }
             case 2:
@@ -102,13 +108,17 @@ int main()
                         Eat.monsterFight(G);
                         break;
                     }
-                    Eat.investigate();
+                    Eat.investigate(G);
+                    Eat.merchant_.displayMenu();
+                    
                 }
                 break;
             }
             case 3:
             {
                 Eat.monsterFight(G);
+                Eat.merchant_.displayMenu();
+                
                 break;
             }
             case 4:
