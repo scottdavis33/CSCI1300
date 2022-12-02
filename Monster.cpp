b7bfb4f1fdc2829d3d8987eed8f0a02e9e6ea85e
scottdavis33 <117961419+scottdavis33@users.noreply.github.com>
2022-12-01 19:24:40 -0700

    Update Monster.cpp

--- Monster.cpp
+++ Monster.cpp
@@ -1,7 +1,7 @@
 // CSCI 1300 Fall 2022
-// Author: Jared Preyer && Scott Davis 
-// Recitation: 105 - Reagan Rychecky && 308 - Baljot Kaur
-// Project 3 - Class Monster
+// Author: Scott Davis
+// Recitation: 308 – Baljot Kaur
+// Project 3 - Class 3
 #include <iostream>
 #include <fstream>
 #include "Monster.h"
@@ -10,34 +10,34 @@
 
 using namespace std;
 
-
+// sets the name to a blank string and the level to 0
 Monster::Monster()
 {
     name_ = "";
     level_ = 0;
 }
-
+// sets the monsters name and level to inputted parameters
 Monster::Monster(string name, int level)
 {
     name_ = name;
     level_ = level;
 }
-
+//sets level
 void Monster::setLevel(int level)
 {
     level_ = level;
 }
-
+//returns level
 int Monster::getLevel()
 {
     return level_;
 }
-
+//sets monster name
 void Monster::setName(string name)
 {
     name_ = name;
 }
-
+// returns monster name
 string Monster::getName()
 {
     return name_;
